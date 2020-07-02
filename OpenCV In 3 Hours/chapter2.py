1ac035f70c43bbbbfd743c21fa465adee2d48b86
Jaewon Lee <joyjwlee@gmail.com>
2020-07-02 11:12:30 -0700

    basic functions

--- OpenCV In 3 Hours/chapter2.py
+++ OpenCV In 3 Hours/chapter2.py
@@ -4,18 +4,18 @@ import cv2
 import numpy as np
 
 img = cv2.imread("Resources/lena.png")
-kernel = np.ones((5,5), np.uint8) # 5 by 5 matrix of ones
+kernel = np.ones((5, 5), np.uint8)  # 5 by 5 matrix of ones
 
-imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to grayscale image
-imgBlur = cv2.GaussianBlur(imgGray, (7,7), 0) # use Gaussian blur function
-imgCanny = cv2.Canny(img, 150, 200) # canny edge detector
-imgDialation = cv2.dilate(imgCanny, kernel, iterations=1) # dilate the edge for canny
-imgEroded = cv2.erode(imgDialation, kernel, iterations=1) # erode, opposite of dilate
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert to grayscale image
+imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 0)  # use Gaussian blur function
+imgCanny = cv2.Canny(img, 150, 200)  # canny edge detector
+imgDialation = cv2.dilate(imgCanny, kernel, iterations=1)  # dilate the edge for canny
+imgEroded = cv2.erode(imgDialation, kernel, iterations=1)  # erode, opposite of dilate
 
 # show the images
-cv2.imshow("Gray Image", imgGray) # gray
-cv2.imshow("Blur Image", imgBlur) # blur
-cv2.imshow("Canny Image", imgCanny) # canny
-cv2.imshow("Dilation Image", imgDialation) # dilated
-cv2.imshow("Eroded Image", imgEroded) # eroded
+cv2.imshow("Gray Image", imgGray)  # gray
+cv2.imshow("Blur Image", imgBlur)  # blur
+cv2.imshow("Canny Image", imgCanny)  # canny
+cv2.imshow("Dilation Image", imgDialation)  # dilated
+cv2.imshow("Eroded Image", imgEroded)  # eroded
 cv2.waitKey(0)
