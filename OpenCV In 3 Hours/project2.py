38b7a107708c1b0d69c9e8c01240830b8b717a80
Jaewon Lee <joyjwlee@gmail.com>
2020-07-02 11:23:34 -0700

    document scanner

--- OpenCV In 3 Hours/project2.py
+++ OpenCV In 3 Hours/project2.py
@@ -5,45 +5,49 @@ import numpy as np
 
 widthImg = 640
 heightImg = 540
-cap = cv2.VideoCapture(0) # webcam
-cap.set(3,widthImg) # width
-cap.set(4,heightImg) # height
-cap.set(10,150) #  brightness
+cap = cv2.VideoCapture(0)  # webcam
+cap.set(3, widthImg)  # width
+cap.set(4, heightImg)  # height
+cap.set(10, 150)  # brightness
+
 
 def preProcessing(img):
-    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
-    imgCanny = cv2.Canny(imgBlur,200,200)
-    kernel = np.ones((5,5)) # matrix of ones
-    imgDilate = cv2.dilate(imgCanny,kernel,iterations=2)
-    imgThres = cv2.erode(imgDilate,kernel,iterations=1)
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+    imgCanny = cv2.Canny(imgBlur, 200, 200)
+    kernel = np.ones((5, 5))  # matrix of ones
+    imgDilate = cv2.dilate(imgCanny, kernel, iterations=2)
+    imgThres = cv2.erode(imgDilate, kernel, iterations=1)
     return imgThres
+
 
 def getContours(img):
     biggest = np.array([])
     maxArea = 0
-    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(
+        img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area>5000:
+        if area > 5000:
             # cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
-            peri = cv2.arcLength(cnt,True)
-            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
             if area > maxArea and len(approx) == 4:
                 biggest = approx
                 maxArea = area
     cv2.drawContours(imgContour, biggest, -1, (255, 0, 0), 20)
     return biggest
 
-def reorder (myPoints):
-    myPoints = myPoints.reshape((4,2))
-    myPointsNew = np.zeros((4,1,2),np.int32)
+
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), np.int32)
     add = myPoints.sum(1)
     # print("add", add)
     myPointsNew[0] = myPoints[np.argmin(add)]
     myPointsNew[3] = myPoints[np.argmax(add)]
-    diff = np.diff(myPoints,axis=1)
-    myPointsNew[1]= myPoints[np.argmin(diff)]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
     myPointsNew[2] = myPoints[np.argmax(diff)]
     # print("NewPoints",myPointsNew)
     return myPointsNew
@@ -52,29 +56,36 @@ def reorder (myPoints):
 def getWarp(img, biggest):
     biggest = reorder(biggest)
     pts1 = np.float32(biggest)
-    pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+    pts2 = np.float32(
+        [[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
     imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
 
-    imgCropped = imgOutput[20:imgOutput.shape[0] - 20, 20:imgOutput.shape[1] - 20]
+    imgCropped = imgOutput[20:imgOutput.shape[0] -
+                           20, 20:imgOutput.shape[1] - 20]
     imgCropped = cv2.resize(imgCropped, (widthImg, heightImg))
 
     return imgCropped
 
-def stackImages(scale,imgArray):
+
+def stackImages(scale, imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
     rowsAvailable = isinstance(imgArray[0], list)
     width = imgArray[0][0].shape[1]
     height = imgArray[0][0].shape[0]
     if rowsAvailable:
-        for x in range ( 0, rows):
+        for x in range(0, rows):
             for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(
+                        imgArray[x][y], (0, 0), None, scale, scale)
                 else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+                    imgArray[x][y] = cv2.resize(
+                        imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2:
+                    imgArray[x][y] = cv2.cvtColor(
+                        imgArray[x][y], cv2.COLOR_GRAY2BGR)
         imageBlank = np.zeros((height, width, 3), np.uint8)
         hor = [imageBlank]*rows
         hor_con = [imageBlank]*rows
@@ -84,21 +95,26 @@ def stackImages(scale,imgArray):
     else:
         for x in range(0, rows):
             if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+                imgArray[x] = cv2.resize(
+                    imgArray[x], (0, 0), None, scale, scale)
             else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
+                imgArray[x] = cv2.resize(
+                    imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2:
+                imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
         ver = hor
     return ver
+
 
 while True:
     success, img = cap.read()
     # img=cv2.flip(img,1)
-    img = cv2.resize(img,(widthImg,heightImg))
+    img = cv2.resize(img, (widthImg, heightImg))
     imgContour = img.copy()
 
-    imgThres = preProcessing(img) # threshold image is preprocessing of original
+    # threshold image is preprocessing of original
+    imgThres = preProcessing(img)
     biggest = getContours(imgThres)
     # if something is detected
     if biggest.size != 0:
