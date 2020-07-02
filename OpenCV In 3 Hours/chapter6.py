404b816dd95e9e075477849ec7e10d006dd77abe
Jaewon Lee <joyjwlee@gmail.com>
2020-07-02 11:19:54 -0700

    joining images

--- OpenCV In 3 Hours/chapter6.py
+++ OpenCV In 3 Hours/chapter6.py
@@ -3,20 +3,25 @@
 import cv2
 import numpy as np
 
-def stackImages(scale,imgArray):
+
+def stackImages(scale, imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
     rowsAvailable = isinstance(imgArray[0], list)
     width = imgArray[0][0].shape[1]
     height = imgArray[0][0].shape[0]
     if rowsAvailable:
-        for x in range ( 0, rows):
+        for x in range(0, rows):
             for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(
+                        imgArray[x][y], (0, 0), None, scale, scale)
                 else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+                    imgArray[x][y] = cv2.resize(
+                        imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2:
+                    imgArray[x][y] = cv2.cvtColor(
+                        imgArray[x][y], cv2.COLOR_GRAY2BGR)
         imageBlank = np.zeros((height, width, 3), np.uint8)
         hor = [imageBlank]*rows
         hor_con = [imageBlank]*rows
@@ -26,13 +31,17 @@ def stackImages(scale,imgArray):
     else:
         for x in range(0, rows):
             if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+                imgArray[x] = cv2.resize(
+                    imgArray[x], (0, 0), None, scale, scale)
             else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
+                imgArray[x] = cv2.resize(
+                    imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2:
+                imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
         ver = hor
     return ver
+
 
 img = cv2.imread("Resources/lena.png")
 
@@ -44,8 +53,8 @@ img = cv2.imread("Resources/lena.png")
 # cv2.imshow("Vertical",imgVer)
 
 # stackImages method allows images to be stacked regardless of channel
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-imgStack = stackImages(0.5,([img,imgGray,img],[imgGray,img,imgGray]))
-cv2.imshow("ImageStack",imgStack)
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgStack = stackImages(0.5, ([img, imgGray, img], [imgGray, img, imgGray]))
+cv2.imshow("ImageStack", imgStack)
 
 cv2.waitKey(0)
