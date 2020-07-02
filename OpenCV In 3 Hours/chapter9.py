ca34c24bf617348dd9c7f2ecf9d5fa9ab912bf60
Jaewon Lee <joyjwlee@gmail.com>
2020-07-02 11:21:10 -0700

    face detection

--- OpenCV In 3 Hours/chapter9.py
+++ OpenCV In 3 Hours/chapter9.py
@@ -2,14 +2,15 @@
 
 import cv2
 
-faceCascade = cv2.CascadeClassifier("Resources/haarcascades/haarcascade_frontalface_default.xml")
+faceCascade = cv2.CascadeClassifier(
+    "Resources/haarcascades/haarcascade_frontalface_default.xml")
 img = cv2.imread("Resources/cra cast.jpg")
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # get faces and loop through each to plot rectangles
-faces = faceCascade.detectMultiScale(imgGray,1.1,4)
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)
--- OpenCV In 3 Hours/project3.py
+++ OpenCV In 3 Hours/project3.py
@@ -3,17 +3,18 @@
 import cv2
 import numpy as np
 
-nPlateCascade = cv2.CascadeClassifier("Resources/haarcascades/haarcascade_russian_plate_number.xml")
-minArea = 500 # avoid noise
-color = (255,0,255)
-count = 0 # count when saving
+nPlateCascade = cv2.CascadeClassifier(
+    "Resources/haarcascades/haarcascade_russian_plate_number.xml")
+minArea = 500  # avoid noise
+color = (255, 0, 255)
+count = 0  # count when saving
 
 widthImg = 640
 heightImg = 480
 cap = cv2.VideoCapture(0)
-cap.set(3,widthImg)
-cap.set(4,heightImg)
-cap.set(10,150)
+cap.set(3, widthImg)
+cap.set(4, heightImg)
+cap.set(10, 150)
 
 while True:
     success, img = cap.read()
@@ -22,20 +23,22 @@ while True:
 
     for (x, y, w, h) in numberPlates:
         area = w*h
-        if area>minArea:
+        if area > minArea:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.putText(img,"Number Plate",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,1,color,2)
-            imgRoi = img[y:y+h,x:x+w]
-            cv2.imshow("ROI",imgRoi)
+            cv2.putText(img, "Number Plate", (x, y-5),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, color, 2)
+            imgRoi = img[y:y+h, x:x+w]
+            cv2.imshow("ROI", imgRoi)
 
     cv2.imshow("Results", img)
 
     # press s to save
     if cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.imwrite("Resources/Scanned/NoPlate_"+str(count)+".jpg",imgRoi)
-        cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
-        cv2.putText(img,"Scan Saved",(150,265),cv2.FONT_HERSHEY_COMPLEX,2,(0,0,255),2)
-        cv2.imshow("Result",img)
+        cv2.imwrite("Resources/Scanned/NoPlate_"+str(count)+".jpg", imgRoi)
+        cv2.rectangle(img, (0, 200), (640, 300), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, "Scan Saved", (150, 265),
+                    cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 2)
+        cv2.imshow("Result", img)
         cv2.waitKey(500)
         count += 1
 
