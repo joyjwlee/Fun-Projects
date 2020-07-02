bc985293a715c225de5edbaba6d64fe686b1b32b
Jaewon Lee <joyjwlee@gmail.com>
2020-07-02 11:19:27 -0700

    warp perspective

--- OpenCV In 3 Hours/chapter5.py
+++ OpenCV In 3 Hours/chapter5.py
@@ -5,12 +5,16 @@ import numpy as np
 
 img = cv2.imread("Resources/cards.jpg")
 
-width,height = 250,350
-pts1 = np.float32([[111,219],[287,188],[154,482],[352,440]]) # points of the corner of card
-pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]]) # points for warped
-matrix = cv2.getPerspectiveTransform(pts1,pts2) # warp matrix
-imgOutput = cv2.warpPerspective(img,matrix,(width,height)) # create the warped image
+width, height = 250, 350
+# points of the corner of card
+pts1 = np.float32([[111, 219], [287, 188], [154, 482], [352, 440]])
+# points for warped
+pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+# warp matrix
+matrix = cv2.getPerspectiveTransform(pts1, pts2)
+# create the warped image
+imgOutput = cv2.warpPerspective(img, matrix, (width, height))
 
-cv2.imshow("Image",img)
-cv2.imshow("Output",imgOutput)
+cv2.imshow("Image", img)
+cv2.imshow("Output", imgOutput)
 cv2.waitKey(0)
