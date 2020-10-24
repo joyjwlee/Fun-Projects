68f6ac74c3ebdb55bf2cc2d335e5ecad063bd25c
Jaewon Lee <joyjwlee@gmail.com>
2020-10-24 13:06:28 -0700

    manually set type pause

--- PyAutoGUI/typebot/aesopType.py
+++ PyAutoGUI/typebot/aesopType.py
@@ -7,8 +7,8 @@ time.sleep(3)
 f = open("aesop.txt", 'r')
 
 for word in f:
-    # for char in word:
-    #    pyautogui.write(char)
-    pyautogui.typewrite(word)
+    for char in word:
+        pyautogui.press(char, _pause=False)
+        time.sleep(0.01)
+    # pyautogui.typewrite(word)
     pyautogui.press("enter")
-    time.sleep(0.1)
