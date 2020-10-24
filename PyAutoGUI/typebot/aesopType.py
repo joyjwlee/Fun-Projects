f4910625f6dce3c5bf53fe8cb9b557cdacc78077
Jaewon Lee <joyjwlee@gmail.com>
2020-10-24 13:03:10 -0700

    char alternate too slow

--- PyAutoGUI/typebot/aesopType.py
+++ PyAutoGUI/typebot/aesopType.py
@@ -7,6 +7,8 @@ time.sleep(3)
 f = open("aesop.txt", 'r')
 
 for word in f:
+    # for char in word:
+    #    pyautogui.write(char)
     pyautogui.typewrite(word)
     pyautogui.press("enter")
     time.sleep(0.1)
